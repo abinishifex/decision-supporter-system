ef4d22e6001bf3ea6ddb0848a714acaa6b6f0e07
Yobsan Fufa <yobsanfufa3@gmail.com>
2026-04-06 15:02:51 +0300

    Final: Make build.sh more robust for Render deployments

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
-cd backend
+# Only CD into backend if we're not already in it
+if [ -d "backend" ]; then
+    cd backend
+fi
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
